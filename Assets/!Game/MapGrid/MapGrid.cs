017335c413f42fda40ec1605b5bca26a9fc25b09
obolulu <ozan.bolulu@yahoo.com>
2024-03-03 01:25:33 +0300

    getRotated function NOT TESTED
    title

--- Assets/!Game/MapGrid/MapGrid.cs
+++ Assets/!Game/MapGrid/MapGrid.cs
@@ -5,11 +5,41 @@ public class MapGrid : MonoBehaviour
     public int length = 5;
     public int height = 5;
 
-
-    public bool hasUpExit = false;
+""""
+   public bool hasUpExit = false;
     public bool hasRightExit = false;
     public bool hasDownExit = false;
     public bool hasLeftExit = false;
+""""
 
-        
+    public ArrayList arrlist = new ArrayList(bool hasUpExit, bool hasRightExit, bool hasDownExit, bool hasLeftExit)
+
+
+
+    public void GetRotated()
+    {
+        bool extraExit = false;
+        int a = 0;
+        foreach (bool i in arrlist)
+        {
+            if(a==4)
+            {
+                if(arrlist[0]==true)
+            }
+            else if (i == true)
+            {
+                if (arrlist[a + 1] == true) { extraExit = true; }
+                i + 1 = true;
+            }
+            else if (extraExit == true)
+            {
+                if (arrlist[a + 1] == false) { extraExit = false; }
+                i + 1 = true;
+            }
+            a++;
+        }
+
+
+    }
+
 }
