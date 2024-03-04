de3cf253573b6add1018cb9df280b9ef313bfc92
kskefeafyonlu <karsiyakaefe@icloud.com>
2024-03-04 19:19:13 +0300

    v0.1.0

--- Assets/!Game/RoomsSystem/RoomsManager.cs
+++ Assets/!Game/RoomsSystem/RoomsManager.cs
@@ -1,27 +1,37 @@
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class RoomsManager : MonoBehaviour
+public class RoomsManager : SerializedMonoBehaviour
 {
 
 
     public int width = 5;
     public int height = 5;
 
+    [TableMatrix(SquareCells = true)]
     public Room[,] RoomsGrid;
 
+    public List<Room> RoomPrefabsList = new List<Room>();
 
 
 
 
-    private void Start() {
+
+
+
+    private void Start()
+    {
         InitalizeRoomsGrid();
     }
 
-    private void Update() {
-        
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            CreateEntryRoom();
+        }
     }
-
-
 
 
 
@@ -33,6 +43,42 @@ public class RoomsManager : MonoBehaviour
     }
 
 
+
+#region PlaceRoomFunctions
+    public void PlaceRoomAt(Vector2Int index)
+    {
+        RoomsGrid[index.x, index.y] = RoomPrefabsList[0];
+    }
+    public void PlaceRoomAt(int indexX, int indexY)
+    {
+        RoomsGrid[indexX, indexY] = RoomPrefabsList[0];
+    }
+#endregion
+
+
+
+
+
+    public void CreateEntryRoom()
+    {
+        int blockadeX = width/2;
+        int blockadeY = height/2;
+
+        PlaceRoomAt(blockadeX, blockadeY);
+    }
+
+
+
+    public void CheckAvailableNeighbours()
+    {
+
+    }
+    public void CheckAvailableNeighbours(int x, int y)
+    {
+
+    }
+    public void CheckAvailableNeighbours(Vector2Int index)
+    {
+
+    }
 }
-
-
